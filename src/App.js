a3083564f59034770a69c2fc59c5bfd1b24c515e
Carlos J Figueroa <71535916+carlosmdq44@users.noreply.github.com>
2022-06-28 21:28:01 -0300

    add landing
    Se agrega cartWidget y ItemListContainer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import NavBar from './components/NavBar';
 import './App.css';
+import ItemListContainer from './ItemListContainer';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <ItemListContainer greeting={"Carlos Figueroa E-commerce"}/>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
